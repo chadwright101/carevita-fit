827e107d3f30e89982562f50aea2882d3f08ca00
Chad Wright <justdeejays@gmail.com>
2023-10-31 09:20:28 +0200

    chore: add packages & remove default next.js data

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+const path = require("path");
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1400],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "the-wright-designs-website-images.s3.af-south-1.amazonaws.com",
+        port: "",
+        pathname: "/carevita-fit/**",
+      },
+    ],
+  },
+};
