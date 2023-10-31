81364ef3da905983a70a5d1118c735ba35d33d2f
Chad Wright <justdeejays@gmail.com>
2023-10-31 09:23:04 +0200

    feat: add util components & init data files

---
+++ app/_components/heading.js
@@ -0,0 +1,19 @@
+const Heading = (
+  cssClasses,
+  children,
+  pageHeading,
+  sectionHeading,
+  subheading
+) => {
+  if (pageHeading) {
+    return <h1 className={`${cssClasses}`}>{children}</h1>;
+  } else if (sectionHeading) {
+    return <h2 className={`${cssClasses}`}>{children}</h2>;
+  } else if (subheading) {
+    return <h3 className={`${cssClasses}`}>{children}</h3>;
+  } else {
+    return null;
+  }
+};
+
+export default Heading;
