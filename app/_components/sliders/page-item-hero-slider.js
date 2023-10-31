38b1dedfce7b2fee6c0afb29799492c76f2712b8
Chad Wright <justdeejays@gmail.com>
2023-10-31 09:26:23 +0200

    feat: add hero slider component & text

---
+++ app/_components/sliders/page-item-hero-slider.js
@@ -0,0 +1,79 @@
+"use client";
+
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+
+import "@splidejs/react-splide/css/core";
+import ImageContainer from "@/app/_components/_hooks/image-container";
+
+const PageItemHeroSlider = ({ imageList }) => {
+  return (
+    <Splide
+      options={{
+        autoplay: true,
+        type: "fade",
+        interval: 5000,
+        rewind: true,
+        dragMinThreshold: { mouse: 50, touch: 150 },
+        breakpoints: {
+          900: {
+            arrows: false,
+          },
+        },
+      }}
+      hasTrack={false}
+    >
+      <div className="splide__arrows splide__arrows--ltr">
+        <button
+          className="splide__arrow splide__arrow--prev"
+          type="button"
+          aria-label="Previous slide"
+          aria-controls="splide02-track"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 40 40"
+            width="40"
+            height="40"
+            focusable="false"
+          >
+            <path d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path>
+          </svg>
+        </button>
+        <button
+          className="splide__arrow splide__arrow--next"
+          type="button"
+          aria-label="Next slide"
+          aria-controls="splide02-track"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 40 40"
+            width="40"
+            height="40"
+            focusable="false"
+          >
+            <path d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path>
+          </svg>
+        </button>
+      </div>
+      <SplideTrack>
+        {imageList.map((url, index) => (
+          <SplideSlide key={index}>
+            <ImageContainer
+              src={url}
+              alt={`CareVita #fit - Image ${index + 1}`}
+              width={1400}
+              height={1000}
+              phone={100}
+              desktopSmall={80}
+              desktop={100}
+              eager={index < 2 ? true : false}
+            />
+          </SplideSlide>
+        ))}
+      </SplideTrack>
+    </Splide>
+  );
+};
+
+export default PageItemHeroSlider;
--- app/page.js
+++ app/page.js
@@ -1,95 +1,82 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import PageItemHeroSlider from "@/app/_components/sliders/page-item-hero-slider";
+
+import data from "@/app/data/general-data.json";
+
+import "@/app/styles/partials/pages/home.module.scss";
+
+const {
+  homePage: {
+    hero: { images, paragraph },
+  },
+} = data;
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <PageItemHeroSlider imageList={images} />
+      <div className="hero-text">
+        <h2 className="white-text">{paragraph[0]}</h2>
+        <h2 className="white-text">{paragraph[1]}</h2>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi nulla
+        repudiandae quasi suscipit at quis officiis est eveniet quia? Esse
+        molestias odio at exercitationem sapiente eveniet error doloribus
+        deserunt earum et vitae officiis quod animi soluta perferendis
+        necessitatibus sint distinctio accusamus ab, autem doloremque. Ratione
+        asperiores perspiciatis explicabo itaque iure quas ipsum et nostrum
+        sapiente. Autem veritatis in et repudiandae est deleniti vitae? Deleniti
+        earum ea molestias! Ipsam accusantium laboriosam est distinctio, alias
+        reprehenderit eius sunt fugiat nemo! Quis rerum quisquam perferendis
+        quod ipsa assumenda dolores aspernatur vel fugiat! Totam ex harum
+        adipisci hic culpa incidunt ratione fugit praesentium excepturi,
+        repellat at. Officiis quibusdam maxime aliquam laboriosam provident
+        architecto accusantium, at, unde odio obcaecati voluptates ratione
+        corrupti quaerat minus tenetur. Debitis quasi at vitae distinctio
+        praesentium, iure non aliquam beatae quod quos doloremque facilis
+        explicabo itaque ut nisi soluta ex eveniet dicta aspernatur sint
+        commodi? Nulla, a ad laudantium illum, laboriosam voluptates inventore
+        eos non animi aliquam vero ea quibusdam unde voluptatem nihil, minus
+        expedita! Adipisci doloribus, et corporis iste tempora reprehenderit
+        necessitatibus sed praesentium? Tempore rem assumenda quia sunt quos
+        alias commodi, vero nisi illum, veniam minima, tempora adipisci
+        obcaecati. Nihil blanditiis nostrum laborum repellat odit repudiandae
+        vitae obcaecati aliquam debitis illum officiis quis qui, eveniet, fuga
+        temporibus voluptas corporis, voluptatem praesentium impedit sint
+        suscipit! Porro minima possimus labore cumque? In, possimus aut placeat
+        nostrum aliquam praesentium temporibus laudantium animi sapiente nobis
+        odit, inventore esse voluptatem, optio veritatis! Corrupti similique
+        alias ut eveniet nihil nobis inventore, dolores qui dolor cumque
+        officiis maiores sequi perferendis. Provident repellat odio omnis
+        voluptatibus iure. Quisquam odit voluptatibus tempora culpa labore
+        aliquam minima nihil quidem facere illum odio distinctio ab molestias,
+        soluta perferendis consequatur vero voluptatum voluptates illo saepe
+        nobis pariatur nam. Excepturi est possimus ad repellat similique quod
+        cum quaerat voluptatem fugit nisi. Error minus repudiandae eligendi
+        porro incidunt minima perspiciatis dolorem ut, labore blanditiis
+        molestias voluptatum nemo tempore nisi accusantium architecto totam
+        vero. Quia vero repellat, ut ducimus unde alias numquam cumque. Deserunt
+        impedit in unde qui ullam commodi ea voluptatibus aspernatur nobis
+        quidem aperiam quia enim, nemo nam voluptas beatae laboriosam incidunt
+        eos sit fuga eaque minima? Commodi hic consectetur distinctio laboriosam
+        suscipit ducimus impedit autem, accusamus excepturi voluptatem! Maiores,
+        reiciendis laboriosam? Est rerum placeat mollitia adipisci explicabo
+        impedit non doloribus, officia voluptatum recusandae pariatur optio
+        numquam quis sed sequi tempora. Perspiciatis omnis, quos sequi
+        consectetur assumenda et vel illum corrupti voluptatibus ea magni amet
+        aperiam nesciunt vitae? Vel eveniet obcaecati asperiores mollitia
+        delectus esse quis tempore aliquid, quod itaque quam, incidunt amet
+        optio dolorum nobis fugit sapiente? Aliquam maxime exercitationem
+        accusamus ullam reprehenderit autem quo distinctio officia tempora eos
+        fugit necessitatibus tenetur placeat suscipit atque voluptate fuga
+        minima repudiandae eligendi beatae, quae accusantium facere aperiam.
+        Molestias, sint et? Nostrum autem quis consequuntur laborum accusantium
+        dicta, velit corrupti earum repudiandae ducimus asperiores animi,
+        consectetur impedit recusandae labore in aspernatur. Ipsam corrupti
+        asperiores molestias magnam perferendis soluta consectetur tempora,
+        autem, nulla debitis, nobis maxime iusto voluptates voluptatibus ipsum
+        ut dolores laboriosam libero.
+      </p>
     </main>
-  )
+  );
 }
