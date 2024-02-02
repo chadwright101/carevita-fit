975104fcfbf9bd6057df46a109d294b656e20b05
Chad Wright <justdeejays@gmail.com>
2024-02-02 12:32:15 +0200

    fix: restore firebase-admin service account data

--- app/_firebase/firebase-admin.js
+++ app/_firebase/firebase-admin.js
@@ -1,17 +1,10 @@
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  let privateKey = process.env.FIREBASE_ADMIN_SDK_PRIVATE_KEY;
-  if (privateKey.includes("\\n")) {
-    privateKey = privateKey.replace(/\\n/g, "\n");
-  }
+  const serviceAccount = process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY;
 
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_ADMIN_SDK_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
-      privateKey: privateKey,
-    }),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
