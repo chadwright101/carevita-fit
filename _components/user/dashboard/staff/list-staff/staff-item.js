9dde5efd5440749eb193571bbde322bdbc8ac006
Chad Wright <justdeejays@gmail.com>
2025-11-06 21:29:18 +0200

    feat: adjust admin staff section button styling

--- _components/user/dashboard/staff/list-staff/staff-item.js
+++ _components/user/dashboard/staff/list-staff/staff-item.js
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { deleteStaffMember, moveStaffToTop } from "../utils/staff-service";
-import classNames from "classnames";
 import { addCacheBustingTimestamp } from "@/_lib/cache-busting-url";
 
 const StaffItem = ({ staff, index, totalStaff, onEdit }) => {
@@ -18,33 +17,53 @@ const StaffItem = ({ staff, index, totalStaff, onEdit }) => {
     <>
       <h3>{staff.name}</h3>
       {staff.image && (
-        <div className="admin-staff__list__item__image">
+        <div className="admin-staff__list__item__image-container">
           <Image src={addCacheBustingTimestamp(staff.image, staff.timestamp || Date.now())} alt={staff.name} width={300} height={300} />
+          <button
+            className="admin-staff__list__item__image-container__edit"
+            type="button"
+            onClick={() => onEdit(staff.id)}
+            aria-label="Edit staff member"
+          >
+            <Image
+              src="/icons/edit.svg"
+              alt="Edit staff member"
+              width={40}
+              height={40}
+            />
+          </button>
+          <button
+            className="admin-staff__list__item__image-container__delete"
+            type="button"
+            onClick={handleDelete}
+            aria-label="Delete staff member"
+          >
+            <Image
+              src="/icons/close-icon2.svg"
+              alt="Delete staff member"
+              width={40}
+              height={40}
+            />
+          </button>
+          {index !== 0 && index < totalStaff && (
+            <button
+              className="admin-staff__list__item__image-container__arrow"
+              type="button"
+              onClick={handleMoveToTop}
+              aria-label="Move staff member to top"
+            >
+              <Image
+                src="/icons/up-arrow.svg"
+                alt="Move staff member to top"
+                width={40}
+                height={40}
+              />
+            </button>
+          )}
         </div>
       )}
 
       <p>{staff.bio}</p>
-      <div
-        className={classNames({
-          "admin-staff__list__item__buttons": index !== 0,
-          "admin-staff__list__item__buttons-first": index === 0,
-        })}
-      >
-        <button onClick={() => onEdit(staff.id)} aria-label="Edit staff member">
-          Edit
-        </button>
-        <button onClick={handleDelete} aria-label="Delete staff member">
-          Delete
-        </button>
-        {index !== 0 && index < totalStaff && (
-          <button
-            onClick={handleMoveToTop}
-            aria-label="Move staff member to top"
-          >
-            Reorder
-          </button>
-        )}
-      </div>
     </>
   );
 };
